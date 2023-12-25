2bc4db9619d8a1a1c365dd99602d8309f19df434
porashor das <porashorbabu@gmail.com>
2023-12-25 16:26:02 +0600

    success

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,13 @@
-let a = 'bangladesh';
-document.querySelector('.head').innerHTML = a;
+const a = [3, 4 ,2, 78, 98 ,12];
+
+
+
+function myfunction(total, value, index, array){
+    console.log(total);
+    return total + value;
+}
+let newarry = a.reduce(myfunction);
+//console.log(newarry)
+
+
+
